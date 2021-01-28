df2719896ef678b19b94d4d66c7b5616491fa4e6
Emmanuel Ojo <emmanuel.ojo97@gmail.com>
2021-01-28 16:44:21 +0000

    exercise 4 complete

--- src/main_test.py
+++ src/main_test.py
@@ -5,3 +5,11 @@ def test_capitalise_string():
     assert capitalise_string("hello") == "HELLO"
     assert capitalise_string("hello world") == "HELLO WORLD"
     assert capitalise_string("today I am 25!") == "TODAY I AM 25!"
+
+def decapitalise_string(str):
+    return str.lower()
+
+def test_decapitalise_string():
+    assert decapitalise_string("HELLO") == "hello"
+    assert decapitalise_string("HELLO WORLD") == "hello world"
+    assert decapitalise_string("TODAY I AM 25!") == "today i am 25!"

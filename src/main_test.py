8f5913d3c7066dd93ab594033acb8b90e93bbbc4
Muhammad Tanvir Bin Ali <ali2912@outlook.com>
2021-01-28 16:35:22 +0000

    exercise 3 complete

--- src/main_test.py
+++ src/main_test.py
@@ -2,4 +2,6 @@ def capitalise_string(str):
     return str.upper()
 
 def test_capitalise_string():
-    assert capitalise_string("hello") == "hello".upper()
+    assert capitalise_string("hello") == "HELLO"
+    assert capitalise_string("hello world") == "HELLO WORLD"
+    assert capitalise_string("today I am 25!") == "TODAY I AM 25!"
